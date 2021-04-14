de3b55cbbca22b0e2f5c6f11b6b811a74cc8f714
Florian Klampfer <mail@qwtel.com>
2021-04-14 14:15:57 +0700

    Add native HTTP

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 # Deno Fetch Event Adapter
 
-Dispatches global `fetch` events using Deno's [http](https://deno.land/std/http) module.
+Dispatches global `fetch` events using Deno's ~~[http](https://deno.land/std/http) module~~ [native HTTP server](https://deno.com/blog/v1.9#native-http%2F2-web-server).
 
 It is mostly intended as a temporary solution until Deno [implements the Service Worker spec](https://github.com/denoland/deno/issues/5957#issuecomment-722568905) directly.
 
 It works fine for local testing, developing Cloudflare Workers while offline, and similar use cases. 
-However, production use is not recommended.
 
 ## Example
 
 ```ts
-// file: 'mod.ts'
-import 'https://deno.land/x/fetch_event_adapter/mod.ts';
+import 'https://deno.land/x/fetch_event_adapter/listen.ts';
 
 if (typeof FetchEvent !== 'undefined') console.log(true);
 
@@ -23,19 +21,18 @@ self.addEventListener('fetch', event => {
 });
 ```
 
-Your script needs the `--allow-net` permission and also requires a `--location`:
+Your script needs the `--unstable` (for native HTTP) and `--allow-net` permissions. It also requires a `--location`, 
+to set the port w
 
 ```sh
 deno run --allow-net --location=http://localhost:8000 mod.ts
 ```
 
 If you set the `--location` to either HTTPS or port 443, you have to provide a `--cert` and a `--key` parameter.
-Your worker will also need the read permission to read the files:
+In that case, your script will also need the read permission to read the files:
 
 ```sh
 deno run --allow-net --allow-read --location=https://localhost:8000 mod.ts \
   --cert=./path/to/localhost.key \
   --key=./path/to/localhost.crt
 ```
-
-For more, see Deno's [`http` module documentation](https://github.com/denoland/deno_std/tree/main/http).
---
+++ listen.ts
@@ -0,0 +1,106 @@
+import * as flags from "https://deno.land/std/flags/mod.ts";
+
+class AdaptedFetchEvent extends Event implements FetchEvent {
+  #event: Deno.RequestEvent;
+
+  constructor(type: string, eventInitDict?: FetchEventInit);
+  constructor(event: Deno.RequestEvent);
+  constructor(event: string | Deno.RequestEvent) {
+    super('fetch');
+    if (typeof event === 'string') throw Error('Overload not implemented');
+    this.#event = event;
+  }
+  get request(): Request { return this.#event.request };
+  respondWith(r: Response | Promise<Response>): void {
+    this.#event.respondWith(r);
+  }
+  waitUntil(_f: any): void {
+    // Deno doesn't shut down the way Service Workers or CF Workers do, so this is a noop.
+  }
+
+  get clientId(): string { return '' };
+  get preloadResponse(): Promise<any> { return Promise.resolve() };
+  get replacesClientId(): string { return '' };
+  get resultingClientId(): string { return '' };
+}
+
+// TODO: Don't overwrite if already present?
+self.FetchEvent = AdaptedFetchEvent;
+
+;(async () => {
+  let server: Deno.Listener<Deno.NetAddr>;
+
+  if (self.location.protocol === 'https:' || self.location.port === '433') {
+    const { c, cert, k, key } = flags.parse(Deno.args);
+    const certFile = cert || c;
+    const keyFile = key || k;
+
+    if (!certFile || !keyFile) {
+      console.warn('When using HTTPS or port 443, a --cert and --key are required.');
+    }
+
+    server = Deno.listenTls({
+      hostname: self.location.hostname,
+      port: Number(self.location.port || 443),
+      certFile,
+      keyFile,
+    });
+  } else {
+    server = Deno.listen({ 
+      hostname: self.location.hostname,
+      port: Number(self.location.port || 80),
+    });
+  }
+
+  for await (const conn of server) {
+    (async () => {
+      for await (const event of Deno.serveHttp(conn)) {
+        self.dispatchEvent(new AdaptedFetchEvent(event));
+      }
+    })();
+  }
+})();
+
+declare global {
+  /** Extends the lifetime of the install and activate events dispatched on the global scope as part of the service worker lifecycle. This ensures that any functional events (like FetchEvent) are not dispatched until it upgrades database schemas and deletes the outdated cache entries. */
+  interface ExtendableEvent extends Event {
+    waitUntil(f: any): void;
+  }
+
+  interface ExtendableEventInit extends EventInit {
+  }
+
+  var ExtendableEvent: {
+    prototype: ExtendableEvent;
+    new(type: string, eventInitDict?: ExtendableEventInit): ExtendableEvent;
+  };
+
+  interface FetchEventInit extends ExtendableEventInit {
+    clientId?: string;
+    preloadResponse?: Promise<any>;
+    replacesClientId?: string;
+    request: Request;
+    resultingClientId?: string;
+  }
+
+  var FetchEvent: {
+    prototype: FetchEvent;
+    new(type: string, eventInitDict: FetchEventInit): FetchEvent;
+  };
+
+  /** This is the event type for fetch events dispatched on the service worker global scope. It contains information about the fetch, including the request and how the receiver will treat the response. It provides the event.respondWith() method, which allows us to provide a response to this fetch. */
+  interface FetchEvent extends ExtendableEvent {
+    readonly clientId: string;
+    readonly preloadResponse: Promise<any>;
+    readonly replacesClientId: string;
+    readonly request: Request;
+    readonly resultingClientId: string;
+    respondWith(r: Response | Promise<Response>): void;
+  }
+
+  interface Window {
+    FetchEvent: new (type: string, eventInitDict: FetchEventInit) => FetchEvent;
+  }
+
+  function addEventListener(type: 'fetch', handler: (event: FetchEvent) => void): void;
+}
